3f2cc69028b18f75a558c0ee0957b0b91cf15465
FlyingJ <jason.m.harrington@gmail.com>
2026-02-15 12:37:13 -0500

    Add cleanInput(), not sure why in repl

---
+++ repl_test.go
@@ -0,0 +1,16 @@
+import (
+    "testing"
+)
+
+func TestCleanInput(t *testing.T) {
+    cases := []struct {
+        input    string
+        expected []string
+    }{
+        {
+            input:    "  hello  world  ",
+            expected: []string{"hello", "world"},
+        },
+        // add more cases here
+    }
+}
