9a370a4bdd483f4561d20760f874685dc1bcdfc6
FlyingJ <jason.m.harrington@gmail.com>
2026-02-18 00:15:10 -0500

    Expand test coverage for repl commands file

--- internal/repl/commands_test.go
+++ internal/repl/commands_test.go
@@ -9,3 +9,19 @@ func TestCommandHelp(t *testing.T) {
 		t.Errorf("Error: commandHelp returned non-nil: %v", err)
 	}
 }
+
+func TestGetCommands(t *testing.T) {
+	commands := GetCommands()
+
+	if _, exists := commands["bogus"]; exists {
+		t.Errorf("Error: bogus command should not exist")
+	}
+
+	if helpCommand, exists := commands["help"]; !exists {
+		t.Errorf("Error: help command should exist")
+	} else {
+		if err := helpCommand.Callback(); err != nil {
+			t.Errorf("Error: help command callback did not return nil")
+		}
+	}
+}
